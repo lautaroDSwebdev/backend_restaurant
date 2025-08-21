762ce1710b913a6bec1725c1baaaf159eb637ec3
Lauta Di Salvo <lautarodisalvo2023@gmail.com>
2025-08-21 07:18:22 -0300

    first change

--- src/main/java/com/example/demo/entity/RoleEntity.java
+++ src/main/java/com/example/demo/entity/RoleEntity.java
@@ -13,6 +13,7 @@ public class RoleEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idRole;
+
     @Enumerated(EnumType.STRING)
     private ERole roleName;
 
